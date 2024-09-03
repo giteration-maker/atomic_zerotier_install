1ca48880dff4cd57dd1b8b3e77c51fdd2d5828a1
Csaba Szegedi <giteration@proton.me>
2024-09-03 22:34:53 +0200

    Added some explanation  comments

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Add ZeroTier GPG key
 curl -s https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg | sudo tee /etc/pki/rpm-gpg/RPM-GPG-KEY-zerotier
 
+# Add ZeroTier repository
 cat << 'EOF' | sudo tee /etc/yum.repos.d/zerotier.repo
 [zerotier]
 name=ZeroTier, Inc. RPM Release Repository
@@ -10,7 +12,11 @@ enabled=1
 gpgcheck=1
 EOF
 
+# Install ZeroTier
 rpm-ostree install zerotier-one
+
+# Apply changes live without requering reboot
 sudo rpm-ostree ex apply-live
 
+# Enable and start ZeroTier service
 sudo systemctl enable --now zerotier-one.service
